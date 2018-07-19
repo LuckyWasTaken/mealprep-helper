3ebd6d9520783af15c8b37bc094d3aa829a84577
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-07-19 13:49:59 +0400

    Done 'dummy' data layout

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipiesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Chicken tikka masala', 'Just a test recipe', 'http://assets.kraftfoods.com/recipe_images/opendeploy/173356_640x428.jpg'),
+    new Recipe('Breakfast Burritos', 'Probably a good idea to make 60ish of those',
+     'https://skinnyms.com/wp-content/uploads/2016/10/Zucchini-and-Egg-Breakfast-Burrito-Recipe.jpg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [new Ingredient('Beans can', 5), new Ingredient('Milk bottle', 1)];
 
   constructor() { }
 
